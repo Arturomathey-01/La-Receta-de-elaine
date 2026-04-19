bde875516da3282131a8bb01c38dda9677a8696d
arturo mathey <matheyjimenezjoseparturo@gmail.com>
2026-04-19 17:39:32 -0600

    quite un punto y coma que interferia en el script

--- script.js
+++ script.js
@@ -549,7 +549,7 @@ try {
   folio = String(orderCounter).padStart(4, "0");
   orderCounter++;
 }
-};
+}
   const fecha = new Date().toLocaleString("es-MX", {
     day: "2-digit", month: "2-digit", year: "numeric",
     hour: "2-digit", minute: "2-digit"
